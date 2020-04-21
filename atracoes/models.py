20872e22c510fb6dfc7c62a61af41012262ec7e7
Evaristo Ferreira <evaristofm@gmail.com>
2020-04-21 19:09:39 -0300

    criando e modelando Atracões

--- atracoes/models.py
+++ atracoes/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from atracoes.models import Atracao
 
-
-class PontoTuristico(models.Model):
+class Atracao(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
-    aprovado = models.BooleanField()
-    atracoes = models.ManyToManyField(Atracao)
+    horario_func = models.TextField()
+    idade_minima = models.IntegerField()
 
     def __str__(self):
         return self.nome
